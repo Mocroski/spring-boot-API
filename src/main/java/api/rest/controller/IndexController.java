af045659d6be0855c0aa187dfa53a95c54eea2d6
Lucas Mocroski <devmocroski@gmail.com>
2022-12-11 18:00:31 -0300

    Criando metodo de busca

--- src/main/java/api/rest/controller/IndexController.java
+++ src/main/java/api/rest/controller/IndexController.java
@@ -1,39 +1,39 @@
 package api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.rest.model.Usuario;
+import api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable (value = "id") Long id) {
+		
+	Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init() {
+	public ResponseEntity<List<Usuario>> usuario () {
 		
-		Usuario usuario = new Usuario();
-		usuario.setId(50L);
-		usuario.setLogin("teste");
-		usuario.setNome("teste");
-		usuario.setSenha("teste");
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(10L);
-		usuario2.setLogin("teste2");
-		usuario2.setNome("teste2");
-		usuario2.setSenha("teste2");
-		
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
-		
-		return new ResponseEntity(usuarios, HttpStatus.OK);
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
 	}
 }
