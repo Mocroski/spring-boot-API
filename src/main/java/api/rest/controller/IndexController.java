162532607e3be84971c1ba240254af83dc5c78dc
Lucas Mocroski <devmocroski@gmail.com>
2022-12-13 08:51:43 -0300

    Criando o methodo com DELETE

--- src/main/java/api/rest/controller/IndexController.java
+++ src/main/java/api/rest/controller/IndexController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,6 +65,13 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 		
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String delete (@PathVariable("id") Long id) {
+			
+		usuarioRepository.deleteById(id);
+		return "ok";
 	}
 	
 }
