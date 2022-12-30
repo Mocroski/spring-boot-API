80e11b997f945c0f3f5ea5978d0c8f4a67c542e8
Lucas Mocroski <devmocroski@gmail.com>
2022-12-30 09:10:43 -0300

    Configurando o CrossOrigin dinamicamente

--- src/main/java/api/rest/SpringrestapiApplication.java
+++ src/main/java/api/rest/SpringrestapiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"api.rest.model"}) //le todas as classe desse package e cria as tabelas "entity" automaticament
@@ -16,10 +18,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableTransactionManagement
 @EnableWebMvc //em um projeto spring é possivel trabalhar com rest e mvc ao memso tempo
 @RestController //saber que é um projeto rest e os controllers irao retornar json
-public class SpringrestapiApplication {
+public class SpringrestapiApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestapiApplication.class, args);
+	}
+	
+	/*mapeamento global*/
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario/**")
+		.allowedMethods("POST")
+		.allowedOrigins("www.cliente01.com.br");
+		//Liberando requisisoes POST para o ususario do dominio www.cliente01.com.br
 	}
 
 }
