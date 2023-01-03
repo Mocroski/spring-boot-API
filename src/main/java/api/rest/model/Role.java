34d1ace669d362aec303ea404d4a2ab994f4496d
Lucas Mocroski <devmocroski@gmail.com>
2023-01-03 09:20:18 -0300

    Primero contato com o Spring Security, configurando a ROLE

---
+++ src/main/java/api/rest/model/Role.java
@@ -0,0 +1,51 @@
+package api.rest.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+@SequenceGenerator(name = "seq_role", sequenceName = "seq_role", allocationSize = 1, initialValue = 1)
+public class Role implements GrantedAuthority{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_role")
+	private Long id;
+	
+	private String nomeRole; /*cargo, funcao etc*/
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeRole() {
+		return nomeRole;
+	}
+
+	public void setNomeRole(String nomeRole) {
+		this.nomeRole = nomeRole;
+	}
+
+	@Override
+	public String getAuthority() { //retorna o nome da role
+		
+		return this.nomeRole;
+	}
+	
+	
+}
