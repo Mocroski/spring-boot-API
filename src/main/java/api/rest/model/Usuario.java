e207ca71e99d0412165751a491f845e7deeb529b
Lucas Mocroski <devmocroski@gmail.com>
2023-01-03 17:59:17 -0300

    Sring Security, Config User

--- src/main/java/api/rest/model/Usuario.java
+++ src/main/java/api/rest/model/Usuario.java
@@ -1,19 +1,28 @@
 package api.rest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+import org.hibernate.annotations.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.ConstraintMode;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-public class Usuario implements Serializable{
+public class Usuario implements UserDetails{
 
 	/**
 	 * 
@@ -33,6 +42,14 @@ public class Usuario implements Serializable{
 	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<Telefone> telefones = new ArrayList<Telefone>();
 
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuario_role", uniqueConstraints = @UniqueConstraint (
+			columnNames = {"usuario_id", "role_id"}, name = "unique_role_user"),
+	joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario",
+	foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role",
+	foreignKey = @ForeignKey(name = "role_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Role> roles;
 	
 	public List<Telefone> getTelefones() {
 		return telefones;
@@ -89,6 +106,48 @@ public class Usuario implements Serializable{
 			return false;
 		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return roles;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return this.login;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 	
 	
